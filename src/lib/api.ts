a4d7af585e0d1a9172670a58a2e52865db6b07bf
Ali Roohan <aliroohan321@gmail.com>
2026-06-23 22:38:08 +0500

    fix: use VITE_API_BASE_URL to match env file variable name

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:4000/api';
-
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:4000/api';
+console.log(BASE_URL)
 export const api = axios.create({
   baseURL: BASE_URL,
   // timeout: 60000,
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
+import dotenv from 'dotenv'
+dotenv.config()
 
 // https://vite.dev/config/
 export default defineConfig({
