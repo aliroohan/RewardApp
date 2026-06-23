77b9b88d8e6fde49e29011d6fc5157736ff563df
Ali Roohan <aliroohan321@gmail.com>
2026-06-23 22:30:44 +0500

    chore: comment out api timeout

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,7 +4,7 @@ const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:4000/api';
 
 export const api = axios.create({
   baseURL: BASE_URL,
-  timeout: 60000,
+  // timeout: 60000,
 });
 
 api.interceptors.request.use((config) => {
